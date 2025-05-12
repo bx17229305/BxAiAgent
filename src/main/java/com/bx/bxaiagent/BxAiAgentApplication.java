988a8da760e9e403b81103c4bc9529e056cbee90
bx <17229305@qq.com>
2025-05-12 20:45:49 +0800

    initial commit

---
+++ src/main/java/com/bx/bxaiagent/BxAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.bx.bxaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BxAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BxAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bx/bxaiagent/BxAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bx.bxaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BxAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
