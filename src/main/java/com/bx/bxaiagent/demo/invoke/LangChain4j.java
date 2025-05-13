057feb0ab6226dd56fd28f8c5eac234e4cc1589b
bx <17229305@qq.com>
2025-05-13 09:12:52 +0800

    接入 ollama

---
+++ src/main/java/com/bx/bxaiagent/demo/invoke/LangChain4j.java
@@ -0,0 +1,18 @@
+// package com.bx.bxaiagent.demo.invoke;
+
+
+// import dev.langchain4j.community.model.dashscope.QwenChatModel;
+// import dev.langchain4j.model.chat.ChatLanguageModel;
+
+// public class LangChain4j {
+
+//   public static void main(String[] args) {
+
+//     ChatLanguageModel qwenChatModel = QwenChatModel.builder()
+//         .apiKey("sk-aea6bea7d4e8404eb576e926c083db0b")
+//         .modelName("qwen-turbo")
+//         .build();
+
+//     System.out.println(qwenChatModel.chat("你是谁？"));
+//   }
+// }
--- src/main/java/com/bx/bxaiagent/demo/invoke/Test2.java
+++ src/main/java/com/bx/bxaiagent/demo/invoke/Test2.java
@@ -1,21 +1,21 @@
-package com.bx.bxaiagent.demo.invoke;
+// package com.bx.bxaiagent.demo.invoke;
 
-import jakarta.annotation.Resource;
-import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-import org.springframework.ai.chat.prompt.Prompt;
-@Component
-public class Test2 implements CommandLineRunner{
-    @Resource
-    ChatModel dashscopeChatModel;
+// import jakarta.annotation.Resource;
+// import org.springframework.ai.chat.model.ChatModel;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.stereotype.Component;
+// import org.springframework.ai.chat.prompt.Prompt;
+// @Component
+// public class Test2 implements CommandLineRunner{
+//     @Resource
+//     ChatModel dashscopeChatModel;
   
-    @Override
-    public void run(String... args) throws Exception {
+//     @Override
+//     public void run(String... args) throws Exception {
        
-      var result = dashscopeChatModel.call(new Prompt("你是谁？")).getResult().getOutput();
-       System.out.println(result);
+//       var result = dashscopeChatModel.call(new Prompt("你是谁？")).getResult().getOutput();
+//        System.out.println(result);
 
 
-    }
-}
+//     }
+// }
